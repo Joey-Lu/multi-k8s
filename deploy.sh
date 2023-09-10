8964bd60a9b555f54153cc9cc047e9a0153385b1
joey <joey.lu.au@gmail.com>
2023-09-10 18:05:29 +1000

    fix docker build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t 99177812/mutli-client:latest -t 99177812/mutli-client:$SHA -f ./client/Dockerfile ./client
-docker build -t 99177812/mutli-server:latest -t 99177812/mutli-server:$SHA -f ./server/Dockerfile ./server
-docker build -t 99177812/mutli-worker:latest -t 99177812/mutli-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t 99177812/multi-client:latest -t 99177812/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t 99177812/multi-server:latest -t 99177812/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t 99177812/multi-worker:latest -t 99177812/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push 99177812/multi-client:latest
 docker push 99177812/multi-server:latest
 docker push 99177812/multi-worker:latest
