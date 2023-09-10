ef9d7a88b74aa7b3c57286972e0ec06b152e1747
joey <joey.lu.au@gmail.com>
2023-09-10 18:17:50 +1000

    fix docker build

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ docker push 99177812/multi-worker:latest
 docker push 99177812/multi-client:$SHA
 docker push 99177812/multi-server:$SHA
 docker push 99177812/multi-worker:$SHA
-
-kubetl apply -f k8s
+ 
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=99177812/multi-server:$SHA
 kubectl set image deployments/client-deployment client=99177812/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=99177812/multi-worker:$SHA
